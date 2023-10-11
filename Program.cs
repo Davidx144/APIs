520046c0f463bd2a56de422d08bffa9e0d311c51
Nelson David Aristizabal Amaya <ndaristizabala@unal.edu.co>
2023-10-11 14:03:19 -0500

    initial controlers

--- Program.cs
+++ Program.cs
@@ -7,18 +7,29 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//aqui se inyenctan las dependencias
+builder.Services.AddScoped<IHelloWorldService>(p=> new HelloWorldService());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+//El orden importa, cualquier request pasa por todos estos
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+//Todos estos on middlewares
 app.UseHttpsRedirection();
 
+//app.UseCors();
+
 app.UseAuthorization();
+
+//app.UseWelcomePage();
+
+//app.UseTimeMiddleware();
 
 app.MapControllers();
 
